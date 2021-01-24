adea47a62ad4caf031cc270060944e2cec263550
alesvaupotic <ales@vaupotic.eu>
2021-01-24 11:31:09 +0100

    working

--- src/background/index.js
+++ src/background/index.js
@@ -2,11 +2,23 @@ console.log("---------------------------------------");
 console.log("Blocking Discord typing ping active ...");
 console.log("---------------------------------------");
 
-chrome.webRequest.onBeforeRequest.addListener(
+let currentState = true;
+let listener;
+
+listener = chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
-    console.log("blocking discord typing");
-    return { cancel: true };
+    console.log("blocking discord typing, currentstate = ", currentState);
+    return { cancel: currentState };
   },
   { urls: ["*://discord.com/api/v8/channels/*/typing"] },
   ["blocking"]
 );
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.storage.sync.get("state", function(data) {
+    currentState = !data.state;
+    console.log("Current state:", currentState);
+    chrome.browserAction.setIcon({ path: currentState ? "discord-block.png" : "discord-unblock.png" });
+    chrome.storage.sync.set({ state: currentState }, function(data) {});
+  });
+});
--- src/popup/Popup.svelte
+++ src/popup/Popup.svelte
@@ -1,1 +1,23 @@
-<main class="p-8">Discord "SomeUser is typing ..." notification blocker is active</main>
+<script>
+  import { onMount } from "svelte";
+
+  let currentState = true;
+
+  chrome.storage.sync.get(["discord-typing-blocker"], function(state) {
+    currentState = state;
+  });
+
+  currentState = !currentState;
+
+  chrome.storage.sync.set({ "discord-typing-blocker": currentState }, function(state) {
+    console.log("Typing blocker set to " + state);
+  });
+
+  onMount(() => {
+    console.log("Mounted");
+  });
+
+  //   chrome.browserAction.setIcon({ path: currentState ? "discord-block.png" : "discord-unblock.png" });
+</script>
+
+<main class="p-8">Discord "SomeUser is typing ..." notification blocker is {currentState}</main>
